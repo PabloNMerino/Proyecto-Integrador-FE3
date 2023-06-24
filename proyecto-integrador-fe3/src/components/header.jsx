15fc3aaeb309ea53d5e143b699adc64ac84dcc3e
PabloNMerino <pablonicolasm@hotmail.com>
2023-06-24 12:53:29 -0300

    first commit

---
+++ proyecto-integrador-fe3/src/components/header.jsx
@@ -0,0 +1,20 @@
+import Style from './headerStyle.module.css'
+import ThemeSwitch from 'react-switch'
+
+const Header = () => {
+
+    return(
+        <header>
+            <p>DentaLink</p>
+            <ul className={Style.lista}>
+                <li>Home</li>
+                <li>Contacto</li>
+                <li>Destacados</li>
+            </ul>
+            <ThemeSwitch offColor='#eef279' onColor='#120f26' offHandleColor='#dbbe00'  onHandleColor='#616063' uncheckedIcon={false} checkedIcon={false}/>
+        </header>
+       
+    )
+}
+
+export default Header
